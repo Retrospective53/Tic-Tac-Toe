6d8815c97e8c12dcf0416d80f92172d9455df0a1
Retrospective53 <harry.pseu53@gmail.com>
2022-12-10 00:31:21 +0700

    added placeMark function in script.js, added styling for x and circle class.

---
+++ sript.js
@@ -0,0 +1,18 @@
+const X_CLASS = 'x';
+const CIRCLE_CLASS = 'circle';
+const cellElements = document.querySelectorAll('[data-cell]');
+let circleTurn
+
+cellElements.forEach(cell => {
+    cell.addEventListener('click', handleClick, { once: true })
+})
+
+function handleClick(e) {
+    const cell = e.target
+    const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+    placeMark(cell, currentClass)
+}
+
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass)
+}
